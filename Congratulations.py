3ee674653b4f04557f7615666733f66fee312282
raj7506 <krkrajan@gmail.com>
2025-09-19 11:03:40 +0530

    Add files via upload
    This is for the project u gave :)

---
+++ Congratulations.py
@@ -0,0 +1,5 @@
+#this is programmes comments a sentence into a uppercase
+text = str(input("enter a positive sentence with lowercase "))
+#.upper helps to make the variable turn upercase
+uppercase_sentence = text.upper()
+print(uppercase_sentence)
