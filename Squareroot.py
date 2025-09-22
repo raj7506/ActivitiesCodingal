9f6dd573246515faddad260e923ed78273733dd7
raj7506 <krkrajan@gmail.com>
2025-09-22 21:17:35 +0530

    Add files via upload

---
+++ Squareroot.py
@@ -0,0 +1,3 @@
+num = int(input("Enter a number to find its square root: "))
+square_root = num ** 0.5
+print("Square root of {} is {}".format(num, square_root))
---
+++ Temp.py
@@ -0,0 +1,7 @@
+temperature = float(input("Enter the current temperature: "))
+cool = 10  
+warm = 20
+if temperature < cool:
+    print("It's cold! So you better wear warm clothes")
+else:
+    print("It's warm! So you better wear light clothes")
