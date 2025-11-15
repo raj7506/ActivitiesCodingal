b8adc51f37f6d500f0fa51617d83daa8f02841a2
raj7506 <krkrajan@gmail.com>
2025-11-15 11:19:12 +0530

    Add files via upload

--- Shuttingdown.py
+++ Shuttingdown.py
@@ -1,2 +1,12 @@
 import os
-os.system(" shutdown /s /t 1")
+def shutdown(answer):
+    
+    if answer == "yes":
+        print("Shutting down...")
+    elif answer == "no":
+        print("Okay! The computer will not shut down")
+    else:
+        print("Sorry, I didn't understand")
+#    os.system("shutdown /s /t 0")
+ans = input("Do you want to shut down your computer? (yes/no): ")
+shutdown(ans)
