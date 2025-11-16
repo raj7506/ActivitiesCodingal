d3a9b04f2ca9ed8c2080c5490d2cfa460b775098
raj7506 <krkrajan@gmail.com>
2025-11-16 10:52:01 +0530

    Add files via upload

---
+++ cost.py
@@ -0,0 +1,4 @@
+total_bill = float(input("Enter the total bill: "))
+amount_paid = float(input("Enter the amount paid: "))
+change = amount_paid - total_bill
+print("The shopkeeper should return: $", change)
