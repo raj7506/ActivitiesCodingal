b29c807fe006eed97cc64b38d9cb17b69f6d4f91
raj7506 <krkrajan@gmail.com>
2025-12-13 12:54:29 +0530

    Add files via upload

---
+++ Set1.py
@@ -0,0 +1,11 @@
+print("\n1st set:")
+Set1 = {'Rose','Sunflower', 'Tulip', 'Jasmine'}
+Set2 = {'Lilies', 'Lavender','Rose','Orchid'}
+print(Set1.symmetric_difference(Set2))
+
+print("\n2nd set:")
+Set1 = {'Apple','Orange', 'Grapes', 'Mango'}
+Set2 = {'Lemon', 'Strawberry','Blueberry','Cherry'}
+Set3 = {'Apple', 'Mango','Watermelon','Grapes'}
+Union_set = Set1.union(Set2, Set3)
+print(Union_set)
